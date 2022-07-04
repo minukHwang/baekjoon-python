ceb03ca960b43e0de02f5c11d66d80e4507c1e0d
minukHwang <104661043+minukHwang@users.noreply.github.com>
2022-07-04 09:51:43 +0900

    [Bronze II] Title: 음계, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,16 +1,11 @@
-music = list(map(int, input().split()))
-ascending = True
-descending = True
+ascending = [1,2,3,4,5,6,7,8]
+descending = [8,7,6,5,4,3,2,1]
 
-for i in range(1, len(music)):
-    if music[i-1] < music[i]:
-        descending = False
-    else:
-        ascending = False
-        
-if ascending:
+music = list(map(int, input().split()))
+
+if music == ascending:
     print("ascending")
-elif descending:
+elif music == descending:
     print("descending")
 else:
     print("mixed")
