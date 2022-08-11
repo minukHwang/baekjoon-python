ab227b0c73eaf0bd032eeace640a30fb791ff193
minukHwang <104661043+minukHwang@users.noreply.github.com>
2022-08-11 15:59:22 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,6 @@
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+word = input()
+
+for item in alphabet:
+    print(word.count(item), end = " ")
